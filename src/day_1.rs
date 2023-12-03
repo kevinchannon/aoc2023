dfdc2974b3ed7844658024c0f570150b150b7ba4
Kevin Channon <kevinchannon@gmail.com>
2023-12-03 10:27:29 +0000

    Day 1, part 1 working

---
+++ src/day_1.rs
@@ -0,0 +1,81 @@
+use std::fs::read_to_string;
+use std::path::Path;
+
+#[derive(PartialEq, Debug)]
+pub enum Error {
+    InvalidInputPaths,
+    ParseLineFailed
+}
+
+pub fn part_1() -> Result<u32, Error> {
+    let lines = get_lines_from_file(Path::new("inputs/day1_part1.txt"))?;
+    lines.iter().map(int_from_line).sum()
+}
+
+fn get_lines_from_file(path: &Path) -> Result<Vec<String>, Error> {
+    if let Ok(content) = read_to_string(path.to_str().ok_or(Error::InvalidInputPaths)?) {
+        return Ok(content.lines().map(String::from).collect());
+    }
+
+    Err(Error::InvalidInputPaths)
+}
+
+fn int_from_line(line: &String) -> Result<u32, Error> {
+    let ten = line.chars()
+        .find(|c| c.is_digit(10))
+        .ok_or(Error::ParseLineFailed)?
+        .to_digit(10)
+        .expect("We only selected base-10 digits in the find, so we shouldn't ever get here");
+    let unit = line.chars()
+        .rfind(|c| c.is_digit(10))
+        .ok_or(Error::ParseLineFailed)?
+        .to_digit(10)
+        .expect("We only selected base-10 digits in the find, so we shouldn't ever get here");
+
+    Ok(10 * ten + unit)
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn int_from_line_works_on_trivial_line() {
+        assert_eq!(Ok(1), int_from_line(&String::from("01")));
+    }
+
+    #[test]
+    fn int_from_line_works_on_line_with_letters() {
+        assert_eq!(Ok(42), int_from_line(&String::from("4a2")));
+        assert_eq!(Ok(42), int_from_line(&String::from("a42")));
+        assert_eq!(Ok(42), int_from_line(&String::from("42a")));
+        assert_eq!(Ok(42), int_from_line(&String::from("a4b2")));
+        assert_eq!(Ok(42), int_from_line(&String::from("a42b")));
+        assert_eq!(Ok(42), int_from_line(&String::from("4a2b")));
+    }
+
+    #[test]
+    fn int_from_line_works_on_line_with_multiple_numbers() {
+        assert_eq!(Ok(42), int_from_line(&String::from("412")));
+    }
+
+    #[test]
+    fn int_from_line_works_on_line_with_single() {
+        assert_eq!(Ok(44), int_from_line(&String::from("4")));
+    }
+
+    #[test]
+    fn int_from_line_fails_with_invalid_parse_error_when_there_are_no_numbers() {
+        assert_eq!(Err(Error::ParseLineFailed), int_from_line(&String::from("a")));
+    }
+
+    #[test]
+    fn int_from_line_works_for_example_lines() {
+        assert_eq!(Ok(12), int_from_line(&String::from("1abc2")));
+        assert_eq!(Ok(38), int_from_line(&String::from("pqr3stu8vwx")));
+        assert_eq!(Ok(15), int_from_line(&String::from("a1b2c3d4e5f")));
+        assert_eq!(Ok(77), int_from_line(&String::from("treb7uchet")));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod day_1;
+
+fn main() {
+    let result_1_1 = day_1::part_1();
+    println!("Calibration value sum: {:?}", result_1_1);
+}
+
