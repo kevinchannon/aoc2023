6f5a799f376a30edd0cc31f167f2d9f31c08c085
Kevin Channon <kevinchannon@gmail.com>
2023-12-03 20:18:56 +0000

    get_calibration_factor converts "one" -> "1" in input strings

--- src/day_1.rs
+++ src/day_1.rs
@@ -8,8 +8,11 @@ pub enum Error {
 }
 
 pub fn get_calibration_factor() -> Result<u32, Error> {
-    let lines = get_lines_from_file(Path::new("inputs/day1_part1.txt"))?;
-    lines.iter().map(int_from_line).sum()
+    get_lines_from_file(Path::new("inputs/day1_part1.txt"))?
+        .iter()
+        .map(words_to_digit_chars)
+        .map(int_from_line)
+        .sum()
 }
 
 fn get_lines_from_file(path: &Path) -> Result<Vec<String>, Error> {
@@ -20,7 +23,11 @@ fn get_lines_from_file(path: &Path) -> Result<Vec<String>, Error> {
     Err(Error::InvalidInputPaths)
 }
 
-fn int_from_line(line: &String) -> Result<u32, Error> {
+fn words_to_digit_chars(s: &String) -> String {
+    s.replace("one", "1")
+}
+
+fn int_from_line(line: String) -> Result<u32, Error> {
     let ten = line.chars()
         .find(|c| c.is_digit(10))
         .ok_or(Error::ParseLineFailed)?
@@ -35,47 +42,50 @@ fn int_from_line(line: &String) -> Result<u32, Error> {
     Ok(10 * ten + unit)
 }
 
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn int_from_line_works_on_trivial_line() {
-        assert_eq!(Ok(1), int_from_line(&String::from("01")));
+        assert_eq!(Ok(1), int_from_line(String::from("01")));
     }
 
     #[test]
     fn int_from_line_works_on_line_with_letters() {
-        assert_eq!(Ok(42), int_from_line(&String::from("4a2")));
-        assert_eq!(Ok(42), int_from_line(&String::from("a42")));
-        assert_eq!(Ok(42), int_from_line(&String::from("42a")));
-        assert_eq!(Ok(42), int_from_line(&String::from("a4b2")));
-        assert_eq!(Ok(42), int_from_line(&String::from("a42b")));
-        assert_eq!(Ok(42), int_from_line(&String::from("4a2b")));
+        assert_eq!(Ok(42), int_from_line(String::from("4a2")));
+        assert_eq!(Ok(42), int_from_line(String::from("a42")));
+        assert_eq!(Ok(42), int_from_line(String::from("42a")));
+        assert_eq!(Ok(42), int_from_line(String::from("a4b2")));
+        assert_eq!(Ok(42), int_from_line(String::from("a42b")));
+        assert_eq!(Ok(42), int_from_line(String::from("4a2b")));
     }
 
     #[test]
     fn int_from_line_works_on_line_with_multiple_numbers() {
-        assert_eq!(Ok(42), int_from_line(&String::from("412")));
+        assert_eq!(Ok(42), int_from_line(String::from("412")));
     }
 
     #[test]
     fn int_from_line_works_on_line_with_single() {
-        assert_eq!(Ok(44), int_from_line(&String::from("4")));
+        assert_eq!(Ok(44), int_from_line(String::from("4")));
     }
 
     #[test]
     fn int_from_line_fails_with_invalid_parse_error_when_there_are_no_numbers() {
-        assert_eq!(Err(Error::ParseLineFailed), int_from_line(&String::from("a")));
+        assert_eq!(Err(Error::ParseLineFailed), int_from_line(String::from("a")));
     }
 
     #[test]
     fn int_from_line_works_for_example_lines() {
-        assert_eq!(Ok(12), int_from_line(&String::from("1abc2")));
-        assert_eq!(Ok(38), int_from_line(&String::from("pqr3stu8vwx")));
-        assert_eq!(Ok(15), int_from_line(&String::from("a1b2c3d4e5f")));
-        assert_eq!(Ok(77), int_from_line(&String::from("treb7uchet")));
+        assert_eq!(Ok(12), int_from_line(String::from("1abc2")));
+        assert_eq!(Ok(38), int_from_line(String::from("pqr3stu8vwx")));
+        assert_eq!(Ok(15), int_from_line(String::from("a1b2c3d4e5f")));
+        assert_eq!(Ok(77), int_from_line(String::from("treb7uchet")));
+    }
+
+    #[test]
+    fn words_to_digit_chars_replaces_digit() {
+        assert_eq!("1".to_string(), words_to_digit_chars(&"one".to_string()));
     }
 }
