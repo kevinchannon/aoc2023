7988ce640c59c3539b002ad01c540944ac3ac9c7
Kevin Channon <kevinchannon@gmail.com>
2023-12-06 07:20:16 +0000

    Day 2(a): Add test for multiple valid games

--- src/day_2.rs
+++ src/day_2.rs
@@ -6,6 +6,7 @@ pub enum Error {
 pub fn get_id_sum() -> u32 {
     0
 }
+
 fn get_id_sum_from_lines(lines: Vec<String>) -> Result<u32, Error> {
     lines.iter().map(id_from_line).sum()
 }
@@ -18,11 +19,18 @@ fn id_from_line(line: &String) -> Result<u32, Error> {
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
     fn get_id_sum_returns_id_of_single_possible_game() {
         assert_eq!(Ok(1), get_id_sum_from_lines(vec![String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")]));
+    }
+
+    #[test]
+    fn get_id_sum_returns_id_of_multiple_possible_games() {
+        let lines = vec![String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+                         String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")];
+        assert_eq!(Ok(3), get_id_sum_from_lines(lines));
     }
 }
