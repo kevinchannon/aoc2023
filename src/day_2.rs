b444496b18d3f02d9beeb24ba617291c017ee01b
Kevin Channon <kevinchannon@gmail.com>
2023-12-08 09:19:55 +0000

    Day 2(a): wip

--- src/day_2.rs
+++ src/day_2.rs
@@ -44,6 +44,41 @@ impl Draw {
     }
 }
 
+#[derive(PartialEq, Debug)]
+struct Game {
+    id: u32,
+    draws: Vec<Draw>
+}
+
+impl Game {
+    pub fn from_string(s: &str) -> Result<Self, Error> {
+        let (id, draws) = Game::get_id_and_draws_str(s)?;
+        let out = Game{id, draws: Game::parse_draws(draws)?};
+
+        Ok(out)
+    }
+
+    fn get_id_and_draws_str(s: &str) -> Result<(u32, &str), Error> {
+        let id_start = s.find(" ").ok_or(Error::FailedToParseId)? + 1;
+        let id_end = s.find(":").ok_or(Error::FailedToParseId)?;
+
+        if let Ok(id) = s[id_start..id_end].parse::<u32>() {
+            return Ok((id, &s[id_end + 1..]));
+        }
+
+        Err(Error::FailedToParseId)
+    }
+
+    fn parse_draws(s: &str) -> Result<Vec<Draw>, Error> {
+        let mut out = Vec::<Draw>::new();
+        for draw_str in s.split(";") {
+            out.push(Draw::from_string(draw_str)?);
+        }
+
+        Ok(out)
+    }
+}
+
 pub fn get_id_sum() -> Result<u32, Error> {
     get_id_sum_from_lines(get_lines_from_file(Path::new("inputs/day2.txt"))?)
 }
@@ -54,6 +89,13 @@ fn get_lines_from_file(path: &Path) -> Result<Vec<String>, Error> {
     }
 
     Err(Error::InvalidInputPaths)
+}
+
+fn games_from_lines(lines: &Vec<String>) -> Result<Vec<Game>, Error> {
+    lines.iter().try_fold(Vec::<Game>::new(), |mut out, l| {
+        out.push(Game::from_string(l.as_str())?);
+        Ok(out)
+    })
 }
 
 fn get_id_sum_from_lines(lines: Vec<String>) -> Result<u32, Error> {
@@ -164,5 +206,35 @@ mod tests {
     #[test]
     fn create_draw_from_string_fails_if_there_are_too_many_parts() {
         assert_eq!(Err(Error::InvalidDraw), Draw::from_string(" 2 green X, 3 yellow, 1 red"));
+    }
+
+    #[test]
+    fn create_game_from_string() {
+        let expected = Game{id: 1, draws: vec![Draw{red: 4, green: 0, blue: 3}, Draw{red: 1, green: 2, blue: 6}, Draw{red: 0, green: 2, blue: 0}]};
+        assert_eq!(Ok(expected), Game::from_string("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"));
+    }
+
+    #[test]
+    fn create_game_from_string_fails_if_id_is_bad() {
+        assert_eq!(Err(Error::FailedToParseId), Game::from_string("Game ?: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"));
+    }
+
+    #[test]
+    fn create_game_from_string_fails_if_draws_are_bad() {
+        assert_eq!(Err(Error::InvalidDraw), Game::from_string("Game 2: wibble"));
+    }
+
+    #[test]
+    fn games_from_lines_works_for_good_games() {
+        let lines = vec![
+            String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"),
+            String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"),
+            String::from("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"),
+            String::from("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"),
+            String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green")
+        ];
+
+        let games = games_from_lines(&lines);
+        assert!(games.is_ok());
     }
 }
